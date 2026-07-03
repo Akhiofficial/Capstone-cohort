eea81e223ab4af081b7e14dd25f57cd65108a814
Akhilesh Mandawgane <akhileshmandawganeofficial@gmail.com>
2026-07-03 15:08:09 +0530

    feat: implement AI orchestration service with Kubernetes deployment, RBAC configuration, and Skaffold sync support

--- ai-orchestration/src/app.js
+++ ai-orchestration/src/app.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
+
+app.get("/api/status/healthz", (req, res) => {
+    res.status(200).json({ message: "AI Service is running", status: "ok" });
+})
+
 app.get("/api/ai/health", (req, res) => {
     res.status(200).json({ message: "Agent is Live" })
 })
